73621775e0b993e93d4dbb1c422608e99ce2482e
fbock <root@uds-500446.ad.unistra.fr.u-strasbg.fr>
2018-03-15 17:43:48 +0100

    Fin Exercice 1

---
+++ src/main/java/fr/unistra/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,27 @@
+package fr.unistra.fizzbuzz;
+
+public class FizzBuzz {
+
+	public String convert(int number) {
+		if(number % 3 == 0)
+		{
+		  
+		  if(number % 5 == 0)
+		  {
+		    return "FizzBuzz";
+		  }
+		  else
+		  {
+		    return "Fizz";
+		  }
+		}
+		if(number % 5 == 0)
+		{
+		  return "Buzz";
+		}
+		else
+		{
+		  return ""+number;
+		}
+	}
+}
